0257c1bf7d5986b5c44311eb384f090ab53e06da
kulakov <alexkharchuk2004@gmail.com>
2025-02-25 12:27:54 +0000

    sneakers shop for practice

---
+++ src/pages/Home.vue
@@ -0,0 +1,166 @@
+<script setup>
+import { reactive, watch, ref, onMounted } from 'vue'
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+import { inject } from 'vue'
+import CardList from '../components/CardList.vue'
+import Card from '../components/Card.vue'
+const { cart, addToCart, removeFromCart } = inject('cart')
+
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onClickAddPlus = (item) => {
+  if (!item.isAdded) {
+    addToCart(item)
+  } else {
+    removeFromCart(item)
+  }
+}
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = debounce((event) => {
+  filters.searchQuery = event.target.value
+}, 300)
+
+const addToFavorite = async (item) => {
+  try {
+    if (!item.isFavorite) {
+      const obj = {
+        item_id: item.id
+      }
+
+      item.isFavorite = true
+
+      const { data } = await axios.post(``, obj)
+
+      item.favoriteId = data.id
+    } else {
+      item.isFavorite = false
+      await axios.delete(`${item.favoriteId}`)
+      item.favoriteId = null
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchFavorites = async () => {
+  try {
+    const { data: favorites } = await axios.get(``)
+
+    items.value = items.value.map((item) => {
+      const favorite = favorites.find((favorite) => favorite.item_id === item.id)
+
+      if (!favorite) {
+        return item
+      }
+
+      return {
+        ...item,
+        isFavorite: true,
+        favoriteId: favorite.id
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(``, {
+      params
+    })
+
+    items.value = data.map((obj) => ({
+      ...obj,
+      isFavorite: false,
+      favoriteId: null,
+      isAdded: false
+    }))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(async () => {
+  const localCart = localStorage.getItem('cart')
+  cart.value = localCart ? JSON.parse(localCart) : []
+
+  await fetchItems()
+  await fetchFavorites()
+
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: cart.value.some((cartItem) => cartItem.id === item.id)
+  }))
+})
+
+watch(cart, () => {
+  items.value = items.value.map((item) => ({
+    ...item,
+    isAdded: false
+  }))
+})
+
+watch(filters, fetchItems)
+</script>
+
+<template>
+  <div class="flex justify-between items-center">
+    <h2 class="text-3xl font-bold mb-8">All sneakers</h2>
+
+    <div class="flex gap-4">
+      <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+        <option value="name">By name</option>
+        <option value="price">For the price (cheap)</option>
+        <option value="-price">For the price (expensive)</option>
+      </select>
+
+      <div class="relative">
+        <img class="absolute left-4 top-3" src="/search.svg" />
+        <input
+          @input="onChangeSearchInput"
+          class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+          type="text"
+          placeholder="Find..."
+        />
+      </div>
+    </div>
+  </div>
+
+  <div class="mt-10">
+    <CardList :items="items" @add-to-favorite="addToFavorite" @add-to-cart="onClickAddPlus">
+      <card id=1,
+            title='Nike blazer Mid'
+            price=123,
+            imageUrl="/sneakers/sneakers-1.jpg"
+      ></card>
+      <card id=2,
+            title='Nike blazer Mid'
+            price=223,
+            imageUrl="/sneakers/sneakers-2.jpg"
+      ></card>
+      <card id=3,
+            title='Nike blazer Mid'
+            price=323,
+            imageUrl="/sneakers/sneakers-3.jpg"
+      ></card>
+    </CardList>
+  </div>
+</template>
